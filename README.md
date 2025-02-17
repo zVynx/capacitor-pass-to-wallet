50e29f1f0d67b786783ab9bcecfd2a73807f7a06
Cancer, Rosa Maria <rcancer@deloitte.es>
2025-02-17 15:52:45 +0100

    feat: Update to Capacitor 7

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ https://github.com/NitnelavAH/IonicAdd2Wallet
 | 4.x  | 1.x  |
 | 5.x  | 2.x |
 | 6.x  | 3.x |
+| 7.x  | 4.x |
 
 ## Install
 
--- src/definitions.ts
+++ src/definitions.ts
@@ -1,5 +1,6 @@
 export interface CapacitorPassToWalletPlugin {
   addToWallet(options: { base64: string }): Promise<{ message: string }>;
-  addMultipleToWallet(options: { base64: string[] }): Promise<{ message: string }>;
+  addMultipleToWallet(options: {
+    base64: string[];
+  }): Promise<{ message: string }>;
 }
-
